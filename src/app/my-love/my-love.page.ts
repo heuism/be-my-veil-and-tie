5e5207680a0c7b3b143f4b2384c17f757a38ddef
Hien Curve <hien@curvetomorrow.com.au>
2024-02-11 18:35:48 +1100

    [add-screen] update the naming

---
+++ src/app/my-love/my-love.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-love',
+  templateUrl: 'my-love.page.html',
+  styleUrls: ['my-love.page.scss']
+})
+export class MyLovePage {
+
+  constructor() {}
+
+}
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -8,27 +8,23 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'my-love',
+        loadChildren: () => import('../my-love/my-love.module').then(m => m.MyLovePageModule)
       },
       {
-        path: 'tab2',
+        path: 'about-me',
         loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
       },
       {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
-      },
-      {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/my-love',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/my-love',
     pathMatch: 'full'
   }
 ];
